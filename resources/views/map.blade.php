cb880cb5e1836ff6dbbd2db87a39c7e99ca151fd
Jun Yi Hee <junyi.hjy@gmail.com>
2015-09-16 10:06:20 +0800

    Update map choropleth

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -8,7 +8,19 @@
 	<meta name="description" content="D3">
 	<meta name="author" content="Hee Jun Yi">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/d3.slider.css">
+	<style>
+		.wrapper {
+			width: 800px;
+			margin-top: 70px;
+			margin-left: auto;
+			margin-right: auto;
+		}
 
+		#date {
+			text-align: center;
+		}
+	</style>
 </head>
 
 <body>
@@ -16,36 +28,93 @@
 		<h1>Jobsbank</h1>
 	</header>
 
-	<div id="container" style="position: relative; width: 100%; height: 900px;"></div>
-
+	<div id="container" style="position: relative; width: 100%; height: 700px;"></div>
+	<div class="wrapper">
+		<div id="slider"></div>
+		<div id="date">2014</div>
+	</div>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/queue-async/1.0.7/queue.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+	<script src="/js/d3.slider.js"></script>
 	<script src="/js/datamaps.none.min.js"></script>
 	<script>
+	queue()
+	    .defer(d3.json, "/fyp/location_by_date")
+	    .await(ready);
+
+	var array = [25, 50, 75, 100, 125, 150];
+
+	var color = d3.scale.threshold()
+    		.domain(array)
+    		.range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+
+    var colorKey = d3.scale.threshold()
+    		.domain(array)
+    		.range(array);
+
+	function ready(error, json){
+		console.log(json);
+
+		reverseMap = {};
+		_.each(json, function(list, key, map){
+			_.each(list, function(value, child_index, child_list){
+				if (!(value.date in reverseMap)){
+					reverseMap[value.date] = {};
+				}
+
+				newKey = key.split(" ")[0];
+
+				if (newKey[0] == 'D'){
+					reverseMap[value.date][newKey] = {
+						fillKey: colorKey(value.count),
+						name: key,
+						count: value.count
+					};
+				}
+			})
+		})
+
+		fills = _.object(color.domain(), color.range());
+		fills['defaultFill'] = 'green';
+
+		data = reverseMap[Object.keys(reverseMap)[0]];
+		console.log(Object.keys(reverseMap));
+		console.log(Math.min.apply(null, Object.keys(reverseMap)))
+
+		var dates = _.map(Object.keys(reverseMap), function(i){
+			return new Date(i);
+		}).sort();
+
+		var format = d3.time.format("%Y-%m-%d");
+
+	    d3.select('#slider')
+	    	.call(d3.slider()
+	    		.min(0)
+	    		.max(dates.length - 1)
+	    		.step(1)
+	    		.on("slide", function(event, value){
+	    			d3.select('#date').html(format(dates[value]));
+					data = reverseMap[Object.keys(reverseMap)[value]];
+					map.updateChoropleth(data);
+	    		}));
+
 	    var map = new Datamap({
 	        element: document.getElementById('container'),
 	        geographyConfig: {
-	            dataUrl: '/data/district.topojson'
+	            dataUrl: '/data/district.topojson',
+	            popupTemplate: function(geo, data) {
+	                return ['<div class="hoverinfo"><strong>',
+	                        'District ' + data.name + '<br/>',
+	                        'Count: ' + data.count,
+	                        '</strong></div>'].join('');
+	            }
 	        },
 	        scope: 'districts',
 	        aspectRatio: 0.5625,
-	        fills: {
-	            HIGH: '#afafaf',
-	            LOW: '#123456',
-	            MEDIUM: 'blue',
-	            UNKNOWN: 'rgb(0,0,0)',
-	            defaultFill: 'green'
-	        },
-	        data: {
-	        	'D22': {
-	                fillKey: 'MEDIUM',
-	                districts: 100
-	            },
-	            'D24': {
-	                fillKey: 'LOW',
-	                districts: 1
-	            }
-	        },
+	        fills: fills,
+	        data: data,
 	        // projection: 'mercator',
 	        setProjection: function(element, options) {
 	            var projection, path;
@@ -70,6 +139,7 @@
 	    });
 
 	    map.legend();
-</script>
+	}
+	</script>
 </body>
 </html>
